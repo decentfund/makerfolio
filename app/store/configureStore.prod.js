b71f11bbe50d8cb0f9385860005f3960b5df96e6
David Mihal <david@dmihal.com>
2018-07-07 16:08:50 +0200

    Persist state to localstorage & remove immutable

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,14 +1,20 @@
 // @flow
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, composeEnhancers } from 'redux';
 import thunk from 'redux-thunk';
 import { createHashHistory } from 'history';
 import { routerMiddleware } from 'react-router-redux';
+import persistState from 'redux-localstorage';
 import rootReducer from '../reducers';
 import type { counterStateType } from '../reducers/counter';
 
 const history = createHashHistory();
 const router = routerMiddleware(history);
-const enhancer = applyMiddleware(thunk, router);
+const stateLocalstorage = persistState();
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk, router, stateLocalstorage),
+  stateLocalstorage
+);
 
 function configureStore(initialState?: counterStateType) {
   return createStore(rootReducer, initialState, enhancer);
