521c7053bc99342bf508e79fa2b884f695d2a3b6
Igor Line <linesethenoiz@gmail.com>
2018-07-08 02:10:25 +0200

    Added back link for Add CPD screen

--- app/components/AddCDP.js
+++ app/components/AddCDP.js
@@ -1,5 +1,6 @@
 // @flow
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Home.css';
 
 type Props = {
@@ -14,6 +15,11 @@ export default class AddCDP extends Component<Props> {
 
     return (
       <div>
+        <div className={styles.backButton} data-tid="backButton">
+          <Link to="/">
+            <i className="fa fa-arrow-left fa-3x" />
+          </Link>
+        </div>
         <div className={styles.container} data-tid="container">
           <p>Add CDP Here</p>
           <br />
